4aff787042fe08d0c7cc2f9232c2f2c8ace54e4d
vh13294 <huotvongpanha@gmail.com>
2022-08-30 22:09:55 +0700

    npm i canvas

--- nest/src/app.controller.ts
+++ nest/src/app.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TensorFlowService } from './tensorflow/tensorflow.service';
-import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -15,26 +14,13 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('tensor')
-  getTensor() {
+  @Get('pose')
+  getPose() {
     return this.tensorFlowService.getPose();
   }
 
-  @Get('test')
-  getTest() {
-    return this.tensorFlowService.downloadImage();
+  @Get('draw')
+  async getPoseAndDraw() {
+    await this.tensorFlowService.getPoseAndDraw();
   }
-
-  // @Get('test')
-  // async getImg(@Res() response: Response) {
-  //   const responseImg = await this.tensorFlowService.downloadImage();
-  //   const img = Buffer.from(responseImg.data, 'binary').toString('base64');
-  //   const base64 =
-  //     'data:' + responseImg.headers['content-type'] + ';base64,' + img;
-
-  //   response.writeHead(200, {
-  //     'Content-Type': 'image/png',
-  //   });
-  //   response.end(base64);
-  // }
 }
--- nest/src/tensorflow/tensorflow.service.ts
+++ nest/src/tensorflow/tensorflow.service.ts
@@ -2,40 +2,64 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import {
   createDetector,
+  Pose,
   PoseDetector,
   SupportedModels,
 } from '@tensorflow-models/pose-detection';
 import * as tf from '@tensorflow/tfjs-node';
-import { createWriteStream } from 'fs';
-import { readFile } from 'fs/promises';
+import { createCanvas, loadImage } from 'canvas';
+import { writeFile } from 'fs/promises';
 
 @Injectable()
 export class TensorFlowService implements OnModuleInit {
   constructor(private readonly httpService: HttpService) {}
 
   private detector: PoseDetector;
-
   async onModuleInit() {
     this.detector = await createDetector(SupportedModels.MoveNet);
     console.log(`version tensorflow: ${tf.version['tfjs-core']}`);
   }
 
   async getPose() {
-    // await this.downloadImage();
-
-    const imageBuffer = await readFile('temp/pose.jpg');
-    const tensor = tf.node.decodeJpeg(imageBuffer);
+    const image = await this.downloadImage();
+    const tensor = tf.node.decodeJpeg(image.intArr);
     return await this.detector.estimatePoses(tensor);
   }
 
-  async downloadImage() {
+  async getPoseAndDraw() {
+    const image = await this.downloadImage();
+    const tensor = tf.node.decodeJpeg(image.intArr);
+    const pose = await this.detector.estimatePoses(tensor);
+    await this.canvasDraw(pose[0], image.buffer);
+  }
+
+  private async downloadImage() {
     // accept jpg only 3 channels not 4 ***
     const response = await this.httpService.axiosRef({
-      url: 'jpg',
+      url: 'https://img.freepik.com/premium-photo/group-diverse-friends-taking-selfie-beach_53876-91925.jpg?w=2000',
       method: 'GET',
-      responseType: 'stream',
+      responseType: 'arraybuffer',
     });
 
-    response.data.pipe(createWriteStream('temp/my.jpg'));
+    return {
+      buffer: response.data,
+      intArr: new Uint8Array(response.data),
+    };
+  }
+
+  private async canvasDraw(pose: Pose, buffer: Buffer) {
+    const image = await loadImage(buffer);
+
+    const canvas = createCanvas(image.width, image.height);
+    const context = canvas.getContext('2d');
+
+    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = 'rgba(200, 0, 0, 0.5)';
+    pose.keypoints.forEach((element) => {
+      context.fillRect(element.x, element.y, 50, 50);
+    });
+
+    await writeFile('public/output.jpg', canvas.toBuffer());
   }
 }
