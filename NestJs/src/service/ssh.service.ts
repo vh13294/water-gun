dd3f3b36ef9afea50efb45a629134b934c0ef70e
vh13294 <huotvongpanha@gmail.com>
2023-04-02 17:31:29 +0700

    sshpass

--- NestJs/src/service/ssh.service.ts
+++ NestJs/src/service/ssh.service.ts
@@ -1,57 +1,58 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { NodeSSH } from 'node-ssh';
+import { exec } from 'child_process';
 
 @Injectable()
 export class SshService implements OnModuleInit {
   constructor(private configService: ConfigService) {}
 
-  private ssh = new NodeSSH();
   private url: string;
   private username: string;
   private password: string;
 
   async onModuleInit() {
     this.initConfigs();
-    await this.connectToServer();
     console.log('SSH Service started');
   }
 
   initConfigs() {
-    // this.url = this.configService.get('BASE_URL');
+    // this.url = 192.168.20.12;
     this.url = 'localhost';
     this.username = this.configService.get('SERVER_USERNAME');
     this.password = this.configService.get('SERVER_PASSWORD');
   }
 
-  async connectToServer() {
-    await this.ssh.connect({
-      host: this.url,
-      port: 22,
-      username: this.username,
-      password: this.password,
-    });
-  }
-
   async shutdownServer() {
-    this.ssh
-      .execCommand(
-        `ssh -tt ${this.username}@${this.url} -o StrictHostKeyChecking=no 'sudo shutdown -h now'`,
-      )
-      .then((result) => {
-        console.log('STDOUT: ' + result.stdout);
-        console.log('STDERR: ' + result.stderr);
-      });
+    exec(
+      `sshpass -p ${this.password} ssh -tt ${this.username}@${this.url} -o StrictHostKeyChecking=no 'sudo shutdown -h now'`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+      },
+    );
   }
 
   async rebootServer() {
-    this.ssh
-      .execCommand(
-        `ssh -tt ${this.username}@${this.url} -o StrictHostKeyChecking=no 'sudo shutdown -r now'`,
-      )
-      .then((result) => {
-        console.log('STDOUT: ' + result.stdout);
-        console.log('STDERR: ' + result.stderr);
-      });
+    exec(
+      `sshpass -p ${this.password} ssh -tt ${this.username}@${this.url} -o StrictHostKeyChecking=no 'sudo shutdown -r now'`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+      },
+    );
   }
 }
